674a23dc4a4d45dfa015c08c1fa95cc2f982dec4
alan04003 <89245292+alan04003@users.noreply.github.com>
2022-02-18 10:33:56 +0100

    Add files via upload

---
+++ bubble sort/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+
+namespace bubble_sort
+{
+    class Program
+    {
+      static void Main(string[] args) {
+      
+      Stopwatch stop = Stopwatch.StartNew();
+      stop.Start(); 
+
+	
+		Random rnd = new Random();
+	   int[] arr = new int[5000];
+      for (int j = 0; j < 5000; j++)
+		   {
+			   arr[j]=(rnd.Next(1, 5000)); 
+		   }
+	
+
+         
+         
+         int temp;
+            for (int j = 0; j <= arr.Length - 2; j++) {
+               for (int i = 0; i <= arr.Length - 2; i++) {
+                  if (arr[i] > arr[i + 1]) {
+                     temp= arr[i + 1];
+                     arr[i + 1] = arr[i];
+                     arr[i] = temp;
+                  }
+               }
+            }
+         stop.Stop();
+         
+         Console.WriteLine("Sorted:");
+         foreach (int p in arr)
+         Console.Write(p + " ");
+         Console.WriteLine(" ");  
+
+         Console.WriteLine("Time = " + stop.ElapsedMilliseconds + " ms");
+         Console.Read();
+      }
+   }
+}
+
